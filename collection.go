c4638cd6807a071f4941ed71a4a92f0660ba6684
Guilherme Rocha <rocha.guilherme.rmf@gmail.com>
2025-11-04 18:10:30 -0300

    docs: add docs for collection and array list builder

--- collection.go
+++ collection.go
@@ -2,10 +2,19 @@ package gollections
 
 import "fmt"
 
+// Collection defines the basic operations for a generic data structure.
 type Collection[T any] interface {
+	// Checks if the collection is empty.
 	IsEmpty() bool
+
+	// Returns the collection length (number of elements).
 	Length() int
+
+	// Reverse the collection in place.
 	Reverse()
+
+	// Returns an iterator for the collection.
 	Iterator() Iterator[T]
+
 	fmt.Stringer
 }
--- lists/factory.go
+++ lists/factory.go
@@ -2,13 +2,33 @@ package lists
 
 import (
 	lists "github.com/stochastic-parrots/gollections/internal/lists"
+	"github.com/stochastic-parrots/gollections/internal/lists/array"
 	"github.com/stochastic-parrots/gollections/internal/lists/doublelinked"
 	"github.com/stochastic-parrots/gollections/internal/lists/linked"
 )
 
 type List[T any] = lists.List[T]
 
+// NewArrayList creates and returns a new empty array list.
+//
+// Its a wrapper for golang slices, returns the list as a List[T] interface.
+//
+// Performance Summary (Time Complexity):
+//
+//	Operation                    Time Complexity
+//	--------------------------   ---------------
+//	Append(xs...T)               O(xs) Amortized
+//	Reverse()                    O(N)
+//	Length()                     O(1)
+//	IsEmpty()                    O(1)
+//	String()                     O(N)
+//	Iterator()                   O(1)
+func NewArrayList[T any](size int) List[T] {
+	return array.NewArrayList[T](size)
+}
+
 // NewLinkedList creates and returns a new empty linked list.
+//
 // It returns the list as a List[T] interface.
 //
 // Performance Summary (Time Complexity):
@@ -26,6 +46,7 @@ func NewLinkedList[T any]() List[T] {
 }
 
 // NewDoubleLinkedList creates and returns a new empty double linked list.
+//
 // It returns the list as a List[T] interface.
 //
 // Performance Summary (Time Complexity):
